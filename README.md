2d094ab1b7023067b35b7cec716d3a8976b988db
pooriya <pooriyapeyvandd@gmail.com>
2024-09-01 23:15:55 +0330

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather Application
+
+## Github Link - https://github.com/p-pey/weather
+
+
+## Firebase Link - Unfortunately i couldn't provide a success build due of time limitation ( 5 day was enough, i have another reason  )
+
+
--- landing/webpack.config.js
+++ landing/webpack.config.js
@@ -1,16 +1,17 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   mode: "development",
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
+    filename: "bundle.js",
+    path: __dirname + "/dist",
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
   },
   module: {
     rules: [
@@ -18,31 +19,42 @@ module.exports = {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
-
+      {
+        test: /\.css$/i,
+        use: ["style-loader"],
+      },
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public/*.css",
+          to: '[name][ext]',
+          force: true,
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      inject: 'body',
+      template: path.resolve(__dirname, "public/index.html"),
     }),
     new ModuleFederationPlugin({
-      name: 'landing',
+      name: "landing",
       remotes: {
-        app: 'app@http://localhost:4001/remoteEntry.js',
+        app: "app@http://localhost:4001/remoteEntry.js",
       },
       shared: {
         react: { singleton: true, eager: true },
-        'react-dom': { singleton: true, eager: true },
+        "react-dom": { singleton: true, eager: true },
       },
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public')
+      directory: path.join(__dirname, "public"),
     },
     compress: true,
     port: 4000,
